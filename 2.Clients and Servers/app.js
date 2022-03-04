990505e4c2493f7b014ee9e239b3653914d7788a
Pradhyumna N Holla <pradholla@gmail.com>
2022-03-04 15:46:02 +0530

    Express

--- 2.Clients and Servers/app.js
+++ 2.Clients and Servers/app.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express();
 
+
+app.set('view engine', 'ejs');
+
 // Listen for requests
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    // res.send('<h>Hello World</h>');
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+});
+
+// Redirects
+app.get('about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// 404
+app.use((req, res) => {
+    res.status(404).render('404');
 });
