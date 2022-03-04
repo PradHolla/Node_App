9658ff9c69cd8ad54d43ebb5587bb301cf78c34e
Pradhyumna N Holla <pradholla@gmail.com>
2022-03-04 18:38:48 +0530

    Mongo DB Connection

--- 2.Clients and Servers/app.js
+++ 2.Clients and Servers/app.js
@@ -1,28 +1,68 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blogs');
+// express app
 const app = express();
 
+const dbURI = '';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
 
+// register view engine
 app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
 
-// Listen for requests
-app.listen(3000);
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
+// Mongoose and Mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'Test fgdBlog',
+//         snippet: 'Testasdg snippet',
+//         body: 'Testsaegr body'
+//     })
+//     blog.save()
+//     .then(result => {res.send(result)})
+//     .catch(err => {res.send(err)});
+// });
+
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//     .then(result => {res.send(result)})
+//     .catch(err => {res.send(err)});
+// });
+
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('62220ad71ad0f1d49f31b54e')
+//     .then(result => {res.send(result)})
+//     .catch(err => {res.send(err)});
+// });
+
+
+// Routes
 app.get('/', (req, res) => {
-    // res.send('<h>Hello World</h>');
-    res.render('index');
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-
-    res.render('about');
+  res.render('about', { title: 'About' });
 });
 
-// Redirects
-app.get('about-us', (req, res) => {
-    res.redirect('/about');
+// Blog Routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: 'desc' })
+    .then(result => {res.render('index', { title: 'All Blogs', blogs: result })})
+    .catch(err => {res.send(err)});
 });
 
-// 404
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+  res.status(404).render('404', { title: '404' });
 });
