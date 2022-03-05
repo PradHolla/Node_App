1ac723b6d253c5a17590e045e89b4a4e3cc5855b
Pradhyumna N Holla <pradholla@gmail.com>
2022-03-05 22:36:40 +0530

    Create, Find and Delete

--- 2.Clients and Servers/app.js
+++ 2.Clients and Servers/app.js
@@ -5,7 +5,7 @@ const Blog = require('./models/blogs');
 // express app
 const app = express();
 
-const dbURI = '';
+const dbURI = 'mongodb+srv://prad:prad1234@node-app.h14ul.mongodb.net/node-app?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then((result) => app.listen(3000))
 .catch((err) => console.log(err));
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Mongoose and Mongo sandbox routes
@@ -58,11 +59,30 @@ app.get('/blogs', (req, res) => {
     .catch(err => {res.send(err)});
 });
 
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+	blog.save()
+	.then(result => {res.redirect('/blogs')})
+	.catch(err => {res.send(err)});
+});
+
+app.get('/blogs/:id', (req, res) => {
+	Blog.findById(req.params.id)
+	.then(result => {res.render('details', { blog: result, title: 'Blog Details' })})
+	.catch(err => {res.send(err)});
+});
+
+app.delete('/blogs/:id', (req, res) => {
+	Blog.findByIdAndDelete(req.params.id)
+	.then(result => {res.json({ redirect: '/blogs' })})
+	.catch(err => {res.send(err)});
+});
+
 app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
+	res.render('create', { title: 'Create a new blog' });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+	res.status(404).render('404', { title: '404' });
 });
